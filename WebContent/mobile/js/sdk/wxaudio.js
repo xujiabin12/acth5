130855c0c1ff07d21332ce06ffa788bf431bbc28
xujiabin <jiabin.xu@tingjiandan.com>
2016-02-19 10:25:00 +0800

    开发完成

--- WebContent/mobile/js/sdk/wxaudio.js
+++ WebContent/mobile/js/sdk/wxaudio.js
@@ -6,48 +6,38 @@ var audio = {};
 
 //开始录音
 var startRecord = function(){
-	util.initWxJsAndDo(['startRecord'],function(){
+	util.initWxJsAndDo(['startRecord','stopRecord','uploadVoice'],function(){
 		wx.startRecord();
 	});
 };
 
-var bofang = function(localId){
-	util.initWxJsAndDo(['playVoice'],function(){
-		wx.playVoice({
-			localId: localId // 需要播放的音频的本地ID，由stopRecord接口获得
-		});
-	});
-};
+
 
 //停止录音并且上传
 var stopRecordAndSend = function(){
-	util.initWxJsAndDo(['stopRecord'],function(){
 		wx.stopRecord({
 		    success: function (res) {
 		    	//上传语音
 		    	uploadVoice(res.localId);
 		    }
 		});
-	});
 };
 
-
-
-//监听录音自动停止接口
-var voiceRecordEnd = function(){
-	util.initWxJsAndDo(['onVoiceRecordEnd'],function(){
-		wx.onVoiceRecordEnd({
-		    // 录音时间超过一分钟没有停止的时候会执行 complete 回调
-		    complete: function (res) {
-		      //上传语音
-		    	uploadVoice(res.localId);
+//上传语音接口
+var uploadVoice = function(localId){
+		wx.uploadVoice({
+		    localId: localId, // 需要上传的音频的本地ID，由stopRecord接口获得
+		    isShowProgressTips: 0, // 默认为1，显示进度提示
+		        success: function (res) {
+		         // 返回音频的服务器端ID 
+		        sendVoice(res.serverId);
 		    }
 		});
-	});
 };
-//播放语音接口
+
+//下载并播放语音接口
 var playVoice = function(serverId){
-	util.initWxJsAndDo(['downloadVoice'],function(){
+	util.initWxJsAndDo(['downloadVoice','playVoice'],function(){
 		wx.downloadVoice({
 		    serverId: serverId,
 			isShowProgressTips: 0, // 默认为1，显示进度提示
@@ -58,20 +48,15 @@ var playVoice = function(serverId){
 	});
 	
 };
-
-//上传语音接口
-var uploadVoice = function(localId){
-	util.initWxJsAndDo(['uploadVoice'],function(){
-		wx.uploadVoice({
-		    localId: localId, // 需要上传的音频的本地ID，由stopRecord接口获得
-		    isShowProgressTips: 0, // 默认为1，显示进度提示
-		        success: function (res) {
-		         // 返回音频的服务器端ID 
-		        sendVoice(res.serverId);
-		    }
-		});
+//播放语音
+var bofang = function(localId){
+	wx.playVoice({
+		localId: localId // 需要播放的音频的本地ID，由stopRecord接口获得
 	});
 };
+
+
+
 
 
 //发送语音
@@ -97,8 +82,21 @@ $(function(){
 			return false;
 		}
 	});
-	
-	
-	
 });
 
+
+
+
+
+//监听录音自动停止接口 次接口暂时不用
+var voiceRecordEnd = function(){
+	util.initWxJsAndDo(['onVoiceRecordEnd'],function(){
+		wx.onVoiceRecordEnd({
+		    // 录音时间超过一分钟没有停止的时候会执行 complete 回调
+		    complete: function (res) {
+		      //上传语音
+		    	uploadVoice(res.localId);
+		    }
+		});
+	});
+};
